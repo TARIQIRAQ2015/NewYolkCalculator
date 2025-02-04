241bc7d62622cbdb52f2aea1ebed34905110ca8a
Tariq Al-Yassin <106028337+TARIQIRAQ2015@users.noreply.github.com>
2025-02-04 06:49:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1126,7 +1126,7 @@ st.markdown("""
             <img src="https://cdn-icons-png.flaticon.com/512/3059/3059997.png" alt="Website">
         </a>
         <a href="https://discord.gg/RYDExGGWXh" target="_blank">
-            <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/653714c174fc6c8bbea73caf_636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg" alt="Discord">
+            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968756.png" alt="Discord">
         </a>
         <a href="https://t.me/newyolkfarm" target="_blank">
             <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" alt="Telegram">
